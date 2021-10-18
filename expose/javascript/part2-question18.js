d3ed2ac08eed9ba3092b5a892f7da00c40808be1
Bingrui Guo <b7guo@ucsd.edu>
2021-10-18 01:48:55 -0700

    edit

--- expose/javascript/part2-question18.js
+++ expose/javascript/part2-question18.js
@@ -4,11 +4,4 @@ function displayTime(){
     console.log(time)
 }
 
-//setInterval(displayTime, 1000)
-function printNums(){
-    console.log(1)
-    setTimeout(function(){console.log(2)},1000)
-    setTimeout(function(){console.log(3)},0)
-    console.log(4)
-}
-printNums();
+setInterval(displayTime, 1000)
